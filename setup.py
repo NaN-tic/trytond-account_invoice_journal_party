ec800d52e39dfac70d451dc20d05185c34175a99
C?dric Krier <ced@b2ck.com>
2018-01-10 10:11:35 +0100

    Setup drone and fix setup for test

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# encoding: utf-8
 # This file is part account_invoice_journal_party module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
@@ -12,6 +13,8 @@ try:
 except ImportError:
     from ConfigParser import ConfigParser
 
+MODULE = 'account_invoice_journal_party'
+PREFIX = 'nantic'
 MODULE2PREFIX = {}
 
 
@@ -19,6 +22,7 @@ def read(fname):
     return io.open(
         os.path.join(os.path.dirname(__file__), fname),
         'r', encoding='utf-8').read()
+
 
 def get_require_version(name):
     if minor_version % 2:
@@ -39,8 +43,6 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'nantic_account_invoice_journal_party'
-download_url = 'https://bitbucket.org/nantic/trytond-account_invoice_journal_party'
 
 requires = []
 for dep in info.get('depends', []):
@@ -55,22 +57,22 @@ if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description='Tryton Account Invoice Journal by Party Module',
     long_description=read('README'),
     author='NaN·tic',
     author_email='',
     url='http://www.nan-tic.com/',
-    download_url=download_url,
+    download_url="https://bitbucket.org/nantic/trytond-%s" % MODULE,
     keywords='',
-    package_dir={'trytond.modules.account_invoice_journal_party': '.'},
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.account_invoice_journal_party',
-        'trytond.modules.account_invoice_journal_party.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.account_invoice_journal_party': (info.get('xml', [])
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
             + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
                 'icons/*.svg', 'tests/*.rst']),
         },
@@ -87,9 +89,9 @@ setup(name=name,
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
@@ -100,11 +102,13 @@ setup(name=name,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    account_invoice_journal_party = trytond.modules.account_invoice_journal_party
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
     use_2to3=True,
-    convert_2to3_doctests=['tests/scenario_account_invoice_journal_party.rst'],
+    convert_2to3_doctests=[
+        'tests/scenario_account_invoice_journal_party.rst',
+        ],
     )
