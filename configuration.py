1f7f854249713be98959dbf5c44b7f80854a485a
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 11:15:59 +0200

    Pyflakes fixes

--- configuration.py
+++ configuration.py
@@ -1,14 +1,11 @@
 # This file is part of account_invoice_journal_party module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond import backend
 from trytond.model import ModelSQL, fields
 from trytond.pool import PoolMeta, Pool
 from trytond.pyson import Eval
 
 from trytond.modules.company.model import CompanyValueMixin
-
-__all__ = ['Configuration']
 
 
 class Configuration(metaclass=PoolMeta):
